9f8f31e167a27431009c2c89ae00660049f7fed4
nationalparcel <dolt55@gmail.com>
2019-06-21 12:05:13 -0400

    Add project files.

---
+++ Data/ModelsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class ModelsContext : IdentityDbContext
+    {
+        public ModelsContext(DbContextOptions<ModelsContext> options)
+            : base(options)
+        {
+        }
+    }
+}
